d977babdb58abd301a2b16f547977b652b5f0e25
Yuji YU <roujrouee@gmail.com>
2022-04-12 18:35:03 -0400

    Initial commit

---
+++ src/main/java/com/vega/springit/SpingitApplication.java
@@ -0,0 +1,13 @@
+package com.vega.springit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingitApplication.class, args);
+    }
+
+}
